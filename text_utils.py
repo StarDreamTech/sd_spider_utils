e35f915190c9f140c1f1e3a68989eacb5d347144
cpython666 <2942581284@qq.com>
2025-03-12 00:14:25 +0800

    style(代码格式化)

--- text_utils.py
+++ text_utils.py
@@ -1,10 +1,12 @@
 def normalize_unicode_text(text):
-    '''
+    """
     文本标准化：转换为标准形式
-    '''
+    """
     import unicodedata
+
     normalized_text = unicodedata.normalize("NFKC", text)
-    print('normal', normalized_text)
+    print("normal", normalized_text)
+
 
 def clean_text(text):
     # 替换非断空白符为普通空格
@@ -22,23 +24,23 @@ import re
 
 
 def contains_chinese(text: str) -> bool:
-    '''
+    """
     使用正则表达式检查是否包含汉字
-    '''
+    """
     return bool(re.search(r"[\u4e00-\u9fa5]", text))
 
 
 def contains_date(text: str) -> bool:
-    '''
+    """
     使用正则表达式检查是否包含类似 '2022年03月30日' 的日期
-    '''
-    return bool(re.search(r'\d{4}[-/年]\d{1,2}[-/月]\d{1,2}日?', text))
+    """
+    return bool(re.search(r"\d{4}[-/年]\d{1,2}[-/月]\d{1,2}日?", text))
 
 
-if __name__ == '__main__':
-    '''
+if __name__ == "__main__":
+    """
     文本是否包含中文
-    '''
+    """
     text1 = "This is a te{||||  nmakldnsjdmksxm  15651654 st.把那家伙半小时·"
     text2 = "这是一个测试。"
 
@@ -51,6 +53,6 @@ if __name__ == '__main__':
 
     print(contains_date(text1))  # True
     print(contains_date(text2))  # False
-if __name__ =="__main__":
+if __name__ == "__main__":
     text = "Ｃａｆé['S.\u2009M. Koksbang\xa0', 'S.\u2009M. Koksbang']"  # 包含全角字符和组合字符
     normalize_unicode_text(text)
